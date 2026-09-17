16ed7d151fda812bfd7a1e7235b07a563508e56f
vcastro2022 <vegalira.victor@gmail.com>
2026-09-17 13:28:33 -0600

    Add basic function example

---
+++ funcion_basica.py
@@ -0,0 +1,8 @@
+def crear_saludo(nombre):
+    """Devuelve un saludo personalizado para el nombre recibido."""
+    return f"Hola, {nombre}! Bienvenido a la clase de Python."
+
+
+
+mensaje = crear_saludo("estudiante")
+print(mensaje)
