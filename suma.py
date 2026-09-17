cf2df43e94a355eabc183f5d2a5d02e5dde88909
vcastro2022 <vegalira.victor@gmail.com>
2026-09-17 13:36:08 -0600

    2nd test

---
+++ suma.py
@@ -0,0 +1,8 @@
+def sumar(numero_1, numero_2):
+    """Devuelve la suma de dos números."""
+    return numero_1 + numero_2
+
+
+if __name__ == "__main__":
+    resultado = sumar(5, 3)
+    print(f"El resultado de la suma es: {resultado}")
